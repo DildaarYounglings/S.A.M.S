caba6f1cec07f17e9006cd47e584a55d5049d375
Neonoble <104133057+Neonoble@users.noreply.github.com>
2023-06-30 08:30:39 +0200

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { auth} from '../Firebase/firebase';
 import { signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../Auth/Context/AuthContext';
 
 
 
 const Dashboard = () => {
- 
+  const {user} = useContext(AuthContext)
 
   let navigate = useNavigate();
    // sign out function    
@@ -16,10 +18,10 @@ const Dashboard = () => {
   };
   return (
     <div>
-      <h1>You have successfully logged in</h1>
+      <h1>{user.email} You have successfully logged in</h1>
        <br/>
        <br/>
-       <button onClick={signUserOut} >Log Out</button>
+       <button onClick={signUserOut} style={{color:'white',backgroundColor:'black',width:'200px',height:'50px',borderRadius:'13px',cursor:'pointer'}}>Log Out</button>
     </div>
     
   )
